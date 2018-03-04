fac44800d6362aac385cc3edc650b01e186cd2c6
Mauro Rossi <issor.oruam@gmail.com>
2018-03-04 12:00:44 +0100

    Input: goodix - enable support for GDIX1002 parts
    At least two vendors (Chuwi and Onda) have parts with GDIX1002 id

--- drivers/input/touchscreen/goodix.c
+++ drivers/input/touchscreen/goodix.c
@@ -934,6 +934,7 @@ static UNIVERSAL_DEV_PM_OPS(goodix_pm_ops, goodix_suspend, goodix_resume, NULL);
 
 static const struct i2c_device_id goodix_ts_id[] = {
 	{ "GDIX1001:00", 0 },
+	{ "GDIX1002:00", 0 },
 	{ }
 };
 MODULE_DEVICE_TABLE(i2c, goodix_ts_id);
