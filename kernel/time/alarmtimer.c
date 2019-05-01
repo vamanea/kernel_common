8b636e9c9088d1b10b1b0bd5d639d2c3d3229b8e
Valentin Manea <valentin.manea@gmail.com>
2019-05-01 10:33:26 +0300

    Don't do a rtc wakeup from alarmtimer driver

--- kernel/time/alarmtimer.c
+++ kernel/time/alarmtimer.c
@@ -99,6 +99,7 @@ static int alarmtimer_rtc_add_device(struct device *dev,
 		return -1;
 	if (!device_may_wakeup(rtc->dev.parent))
 		return -1;
+	return -1;
 
 	__ws = wakeup_source_register("alarmtimer");
 
