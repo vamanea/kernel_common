8df0016a6f563d6045d782f2e30122720d019249
Valentin Manea <valentin.manea@gmail.com>
2019-08-23 14:55:56 +0300

    Force alarmtimer to use the test rtc

--- kernel/time/alarmtimer.c
+++ kernel/time/alarmtimer.c
@@ -95,10 +95,10 @@ static int alarmtimer_rtc_add_device(struct device *dev,
 	if (rtcdev)
 		return -EBUSY;
 
-	if (!rtc->ops->set_alarm)
+	if (!rtc->ops->set_alarm || rtc->ops->set_time)
 		return -1;
-	if (!device_may_wakeup(rtc->dev.parent))
-		return -1;
+	/*if (!device_may_wakeup(rtc->dev.parent))
+		return -1;*/
 
 	__ws = wakeup_source_register("alarmtimer");
 
